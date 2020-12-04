f1700b185ba8af7fe354e5b9e9cd99b5da2eb208
汪腾腾 <1751727782@qq.com>
2020-12-04 09:47:26 +0800

    修改config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: wangtengteng
  * @Date: 2020-11-12 17:00:11
- * @LastEditTime: 2020-12-04 09:42:20
+ * @LastEditTime: 2020-12-04 09:46:53
  * @FillPath: Do not edit
  */
 const path = require('path');
@@ -10,6 +10,7 @@ module.exports = {
   outputDir: 'dist',
   /* 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录 */
   assetsDir: "assets",
+  publicPath: './',
   /* 是否在构建生产包时生成 sourceMap 文件，false将提高构建速度 */
   productionSourceMap: false,
   /* 默认情况下，生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存，你可以通过将这个选项设为 false 来关闭文件名哈希。(false的时候就是让原来的文件名不改变) */
@@ -18,7 +19,6 @@ module.exports = {
     host: 'test.cuohe.com',
     port: 8081,
     public: process.env.VUE_APP_PUBLIC_URL,
-    publicPath: './',
     disableHostCheck: true,
     open: true,
     overlay: {
